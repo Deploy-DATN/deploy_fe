ff00f61be75450e6d1a51e87ac00a405bdd43f69
NguyenTTung <cacppkk2211@gmail.com>
2024-09-30 21:20:36 +0700

    fix - kiểm lỗi password new

--- src/pages/ForgotPW/components/newPW.tsx
+++ src/pages/ForgotPW/components/newPW.tsx
@@ -2,7 +2,30 @@ import React, { useState } from 'react';
 import "../styles/ForgotPW.scss";
 
 const NewPW = () => {
+  const [password, setPassword] = useState('');
+  const [passwordCf, setPasswordCf] = useState('');
   const [showPasswordCf, setShowPasswordCf] = useState(false);
+  const [error, setError] = useState('');
+  const [errorcf, setErrorcf] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+    if (!password) {
+      setErrorcf('');
+      setError('Bạn chưa nhập mật khẩu mới');
+    } else if (password.length < 8 || !/[!@#$%^&*]/.test(password) || !/\d/.test(password)) {
+      setErrorcf('');
+      setError('Mật khẩu phải có ít nhất 8 ký tự, chứa ký tự đặc biệt và số');
+    } else if (password !== passwordCf) {
+      setError('');
+      setErrorcf('Mật khẩu xác thực không giống với mật khẩu');
+    } else {
+      setError('');
+      setErrorcf('');
+      // Xử lý code khi 2 mật khẩu giống nhau ở đây
+    }
+  };
 
   return (
     <div className="formverifyfw container-fluid">
@@ -10,18 +33,19 @@ const NewPW = () => {
       <div className="col-lg-6 col-12 ">
             <div className='w-100  d-flex flex-column align-items-center'>
             <h2 className='h2-QMK'>Mật khẩu mới</h2>
-          <form className="w-75 mt-4">
-            <div className="mb-2">
+          <form className="w-75 mt-4" onSubmit={handleSubmit}>
+            <div className="mb-3">
               <label htmlFor="password" className="form-label color-sdt">Mật khẩu mới: </label>
               <input
                 type="password"
                 id="password"
                 autoComplete="off"
-                inputMode="numeric"
-                pattern="[0-9]*"
                 placeholder='Gồm 8 ký tự, có chứa ký tự đặc biệt và ký tự số'
                 className='form-control rounded-pill heightinput-60 boder-color color-pla'
-              />
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                />
+                {error && <div className="text-danger mt-1">{error}</div>}
             </div>
             <div className="mb-4">
                 <label htmlFor="passwordcf" className="form-label color-sdt">Xác nhận mật khẩu: </label>
@@ -32,12 +56,15 @@ const NewPW = () => {
                   autoComplete="off"
                   placeholder='Vui lòng nhập lại mật khẩu'
                   className='form-control rounded-pill heightinput-60 boder-color color-pla'
+                  value={passwordCf}
+                  onChange={(e) => setPasswordCf(e.target.value)}
                 />
                 <i
                   className={`fa-solid ${showPasswordCf ? 'fa-eye-slash' : 'fa-eye'} icon-password`}
                   onClick={() => setShowPasswordCf(!showPasswordCf)}
                 ></i>
                 </div>
+                {errorcf && <div className="text-danger mt-1">{errorcf}</div>}
               </div>
 
             <button type="submit" className="btn heightinput-60 btn-color w-100 mb-3 rounded-pill text-white mt-4 ">Xác nhận</button>
@@ -55,6 +82,7 @@ const NewPW = () => {
         </div>
       </div>
     </div>
+    
   );
 };
 
