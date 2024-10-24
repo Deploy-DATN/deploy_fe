2109a81ce904916f7c6283b004df142cb4e7ef47
NguyenTTung <cacppkk2211@gmail.com>
2024-10-24 22:07:39 +0700

    Giao diện admin
    Thêm, Sửa motelOwner.
    Modal duyệt,từ chối, khóa, mở khóa motel admin.
    Chi tiết motel admin.
    sửa trang thêm nofiti

---
+++ src/pages/admin/motel/component/indexOwner.tsx
@@ -0,0 +1,14 @@
+import "../styles/stylemotel.scss";
+
+export const indexOwner = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12 col-sm-12 col-md-12 col-lg-3 col-xl-3 col-xxl-3 bg-primary h-50">123</div>
+        <div className="col-12 col-sm-12 col-md-12 col-lg-9 col-xl-9 col-xxl-9 bg-success h-50">3534</div>
+      </div>
+    </div>
+  );
+};
+
+export default indexOwner;
--- src/pages/admin/motel/index.tsx
+++ src/pages/admin/motel/index.tsx
@@ -1,47 +1,69 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./styles/stylemotel.scss";
-import { faHouseCircleCheck, faHouseCircleXmark, faHouseLock, faHouseMedicalCircleExclamation, faLaptopHouse, faLock, faLockOpen, faTableCellsRowUnlock, faUnlock, faUnlockAlt, faUnlockKeyhole } from "@fortawesome/free-solid-svg-icons";
-export const Motel = () => {
+import {
+  faHouseCircleCheck,
+  faHouseCircleXmark,
+  faHouseLock,
+  faHouseMedicalCircleExclamation,
+  faUnlock,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import ModalThaotac from "./component/ModalThaotac";
+import { useState } from "react";
+
+export const Motel: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [modalType, setModalType] = useState(""); // Thêm state để xác định loại modal
+
+  const handleOpenModal = (type: string) => {
+    setModalType(type); // Xác định loại modal cần hiển thị
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setModalType("");
+  };
   return (
     <div className="container-fluid">
       <div className="row align-items-stretch">
         <div className="card w-100">
           <div className="card-body p-4">
             <div className="d-flex mb-4 flex-wrap">
-                <a
-                  href="#"
-                  className="btn btn-filter-motel btn-sm px-3 py-2 mx-2 mb-3 btn-transform-y2"
-                >
-                  Tất cả
-                </a>
-                <a
-                  href="#"
-                  className="btn btn-filter-motel btn-sm px-3 py-2 mx-2 mb-3 btn-transform-y2"
-                >
-                  Duyệt
-                </a>
-                <a
-                  href="#"
-                  className="btn btn-filter-motel btn-sm px-3 py-2 mx-2 mb-3 btn-transform-y2"
-                >
-                  Từ chối
-                </a>
-                <a
-                  href="#"
-                  className="btn btn-filter-motel btn-sm px-3 py-2 mx-2 mb-3 btn-transform-y2"
-                >
-                  Mở khóa
-                </a>
-                <a
-                  href="#"
-                  className="btn btn-filter-motel btn-sm px-3 py-2 mx-2 mb-3 btn-transform-y2"
-                >
-                  Khóa
-                </a>
+              <a
+                href="#"
+                className="btn btn-filter-motel btn-sm px-3 py-2 mx-2 mb-3 btn-transform-y2"
+              >
+                Tất cả
+              </a>
+              <a
+                href="#"
+                className="btn btn-filter-motel btn-sm px-3 py-2 mx-2 mb-3 btn-transform-y2"
+              >
+                Duyệt
+              </a>
+              <a
+                href="#"
+                className="btn btn-filter-motel btn-sm px-3 py-2 mx-2 mb-3 btn-transform-y2"
+              >
+                Từ chối
+              </a>
+              <a
+                href="#"
+                className="btn btn-filter-motel btn-sm px-3 py-2 mx-2 mb-3 btn-transform-y2"
+              >
+                Mở khóa
+              </a>
+              <a
+                href="#"
+                className="btn btn-filter-motel btn-sm px-3 py-2 mx-2 mb-3 btn-transform-y2"
+              >
+                Khóa
+              </a>
             </div>
 
             <div className="table-responsive mt-3" data-simplebar>
-              <table className="test-table table table-borderless align-middle text-nowrap rounded-pill">
+              <table className="test-table table table-borderless align-middle text-nowrap">
                 <thead className="">
                   <tr className=" brg-table-tro">
                     <th scope="col">ID</th>
@@ -56,7 +78,9 @@ export const Motel = () => {
                   <tr>
                     <td>ID12345</td>
                     <td>Account1</td>
-                    <td className="text-overflow">123 BMT - Tân an nnnnnn bbbbbbbbb mmmmmmmmm uuuuuuuuu </td>
+                    <td className="text-overflow">
+                      123 BMT - Tân an nnnnnn bbbbbbbbb mmmmmmmmm uuuuuuuuu{" "}
+                    </td>
                     <td>100 phòng</td>
                     <td>
                       <span className="tt-dangthue badge bg-light-success rounded-pill px-3 py-2 fs-3">
@@ -64,19 +88,29 @@ export const Motel = () => {
                       </span>
                     </td>
                     <td>
-                    <a
-                        href="#"
+                      <Link
+                        to="infomotel"
                         className=" px-2 py-1 mx-1 btn-transform-y2"
                       >
-                        <FontAwesomeIcon icon={faHouseMedicalCircleExclamation} size="2xl" color="#298b90" className="icon-table-motel" />
-                      </a>
+                        <FontAwesomeIcon
+                          icon={faHouseMedicalCircleExclamation}
+                          size="2xl"
+                          color="#298b90"
+                          className="icon-table-motel"
+                        />
+                      </Link>
                       <a
                         href="#"
                         className=" px-2 py-1 mx-1 btn-transform-y2"
+                        onClick={() => handleOpenModal("khoa")}
                       >
-                        <FontAwesomeIcon icon={faHouseLock} size="2xl" color="#298b90" className="icon-table-motel" />
+                        <FontAwesomeIcon
+                          icon={faHouseLock}
+                          size="2xl"
+                          color="#298b90"
+                          className="icon-table-motel"
+                        />
                       </a>
-
                     </td>
                   </tr>
                   <tr>
@@ -90,23 +124,37 @@ export const Motel = () => {
                       </span>
                     </td>
                     <td>
-                      <a
-                        href="#"
-                        className=" px-2 py-1 mx-1 btn-transform-y2"
-                      >
-                        <FontAwesomeIcon icon={faHouseMedicalCircleExclamation} size="2xl" color="#298b90" className="icon-table-motel" />
-                        </a>
-                      <a
-                        href="#"
-                        className=" px-2 py-1 mx-1 btn-transform-y2"
-                      >
-                        <FontAwesomeIcon icon={faHouseCircleCheck} size="2xl" color="#298b90" className="icon-table-motel" />
+                      <a href="#" className=" px-2 py-1 mx-1 btn-transform-y2">
+                        <FontAwesomeIcon
+                          icon={faHouseMedicalCircleExclamation}
+                          size="2xl"
+                          color="#298b90"
+                          className="icon-table-motel"
+                        />
                       </a>
                       <a
                         href="#"
                         className=" px-2 py-1 mx-1 btn-transform-y2"
+                        onClick={() => handleOpenModal("duyet")} // Mở modal duyệt
                       >
-                        <FontAwesomeIcon icon={faHouseCircleXmark} size="2xl" color="#298b90" className="icon-table-motel" />
+                        <FontAwesomeIcon
+                          icon={faHouseCircleCheck}
+                          size="2xl"
+                          color="#298b90"
+                          className="icon-table-motel"
+                        />
+                      </a>
+                      <a
+                        href="#"
+                        className=" px-2 py-1 mx-1 btn-transform-y2"
+                        onClick={() => handleOpenModal("tuchoi")}
+                      >
+                        <FontAwesomeIcon
+                          icon={faHouseCircleXmark}
+                          size="2xl"
+                          color="#298b90"
+                          className="icon-table-motel"
+                        />
                       </a>
                     </td>
                   </tr>
@@ -121,19 +169,26 @@ export const Motel = () => {
                       </span>
                     </td>
                     <td>
-                    <a
-                        href="#"
-                        className=" px-2 py-1 mx-1 btn-transform-y2"
-                      >
-                        <FontAwesomeIcon icon={faHouseMedicalCircleExclamation} size="2xl" color="#298b90" className="icon-table-motel" />
-                        </a>
+                      <a href="#" className=" px-2 py-1 mx-1 btn-transform-y2">
+                        <FontAwesomeIcon
+                          icon={faHouseMedicalCircleExclamation}
+                          size="2xl"
+                          color="#298b90"
+                          className="icon-table-motel"
+                        />
+                      </a>
                       <a
                         href="#"
                         className=" px-2 py-1 mx-1 btn-transform-y2"
+                        onClick={() => handleOpenModal("mokhoa")} // Mở modal mở khóa
                       >
-                        <FontAwesomeIcon icon={faUnlock} size="2xl" color="#298b90" className="icon-table-motel" />
+                        <FontAwesomeIcon
+                          icon={faUnlock}
+                          size="2xl"
+                          color="#298b90"
+                          className="icon-table-motel"
+                        />
                       </a>
-
                     </td>
                   </tr>
                 </tbody>
@@ -142,6 +197,9 @@ export const Motel = () => {
           </div>
         </div>
       </div>
+      {showModal && (
+        <ModalThaotac onClose={handleCloseModal} modalType={modalType} />
+      )}
     </div>
   );
 };
