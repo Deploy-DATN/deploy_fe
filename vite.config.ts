a95cc79d72f9231e326cfa25777fcc164ac39a71
nhunhu312 <108985946+nhunhu312@users.noreply.github.com>
2024-10-31 10:10:53 +0700

    config

--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,15 @@ export default defineConfig({
   //     },
   //   },
   // },
+   server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 3000, // you can replace this port with any port
+  },
+  optimizeDeps: {
+    exclude: ["js-big-decimal"],
+  },
 })
