69ab97ac353fc7821b284ec68b1fc9e59151ab1f
NguyenTTung <cacppkk2211@gmail.com>
2024-11-05 20:34:26 +0700

    Revert "tuan-room-part2"
    This reverts commit 7d8cb088b78ed7e435d8ab356f5ac274798e5adc, reversing
    changes made to ab8a28d515824ab5614a3af9ae34bbdcfd0c4fde.

--- src/services/Dto/MotelDto.tsx
+++ src/services/Dto/MotelDto.tsx
@@ -8,17 +8,11 @@ interface PriceInfo {
 interface OwnerInfo {
   id: number;
   fullName: string;
-  phone: string;
-  email: string;
-  avatar: string;
-  createDate: string;
-  status: boolean;
 }
 
-export interface ImageInfo {
-  id?: number;
+interface ImageInfo {
+  id: number;
   link: string;
-  type: string;
 }
 
 export interface MotelDTO {
@@ -31,13 +25,6 @@ export interface MotelDTO {
   price: PriceInfo;
   images: ImageInfo[];
   owner: OwnerInfo;
-}
-
-export interface MotelPaginationResponse {
-  items: MotelDTO[];
-  pageNumber: number;
-  pageSize: number;
-  totalPages: number;
 }
 
 // ------------------------------------------------------------------------------------------------
@@ -53,78 +40,14 @@ export interface MotelByIdDTO {
   lastPrice: PriceInfo;
   images: ImageInfo[];
   owner: OwnerInfo;
-  rooms: MotelRoomDTO[];
-}
-
-export interface MotelRoomDTO {
-  id: number;
-  roomNumber: number;
-  area: number;
-  price: number;
-  lastPrice: number;
-  status: number;
 }
 
 // ------------------------------------------------------------------------------------------------
 
 
-export interface AddMotelAndRoomDTO {
-  name?: string;
-  address?: string;
-  area: string;
-  priceRoom: string;
-  priceElectric: string;
-  priceWater: string;
-  priceOther: string;
-  totalRoom: string;
-  userId: string;
 
-}
 
-export interface GetEditMotelDTO {
-  id: number;
-  name: string;
-  address: string;
-  status: number;
-  createDate: string;
-  totalRoom: number;
-  price: PriceInfo;
-  lastPrice: PriceInfo;
-  images: ImageInfo[];
-  terms: MotelTermDTO[];
-}
 
-export interface EditMotelDTO {
-  name: string;
-  address: string;
-  electric: string;
-  water: string;
-  other: string;
-}
 
-export interface MotelTermDTO {
-  id: number;
-  name: string;
-  type: string;
-  link: string;
-}
 
-// ------------------------------------------------------------------------------------------------
-
-export interface RoomDTO {
-  id: number;
-  roomNumber: number;
-  area: number;
-  price: number;
-  userFromRoom: number;
-  lastPrice: number;
-  status: number;
-}
-
-export interface AddRoomDTO {
-  motelId : string;
-  quantityRoom: string;
-  area: string;
-  price: string;
-}
 
