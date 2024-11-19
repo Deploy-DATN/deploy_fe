eda1917f3917404018f5dd08f579a64e30b12ccc
ho dung <dungho2209@gmail.com>
2024-11-19 15:28:28 +0700

    update trọ nổi bật trọ mới trọ giá rẻ

--- src/pages/admin/notification/component/OwnerNoti.tsx
+++ src/pages/admin/notification/component/OwnerNoti.tsx
@@ -1,5 +1,4 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
 
 export const OwnerIndexNoti: React.FC = () => {
   return (
