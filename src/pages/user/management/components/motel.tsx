3918641e1fe1e5239a33391130a904167df1a24a
ho dung <dungho2209@gmail.com>
2024-11-25 10:03:57 +0700

    rental room user

--- src/pages/user/management/components/motel.tsx
+++ src/pages/user/management/components/motel.tsx
@@ -3,108 +3,130 @@ import 'swiper/css'
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Pagination, Navigation } from 'swiper/modules';
+import { useEffect, useState } from "react";
 
 import '../styles/motel.scss'
-
+import { GetRentalRoomDetailAPI } from '@/services/api/HomeApi';
 const Motel = () => {
+    const [rentalDetail, setRentalDetail] = useState<any | null>(null);
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        const fetchRetalRoomDetail = async () => {
+            try {
+                if (token) {
+                    const response = await GetRentalRoomDetailAPI(token)
+                    setRentalDetail(response.data)
+                    console.log(response)
+                }
+            } catch (error) {
+                console.log("fetch error!!", error)
+            }
+
+        };
+        fetchRetalRoomDetail();
+    }, []);
+
+    console.log(rentalDetail)
     return (
         <div className="container user-motel p-4">
-            <div className='row align-items-center'>
-                <div className='col-5'>
-                    <Swiper
-                        slidesPerView={1}
-                        spaceBetween={10}
-                        loop={true}
-                        pagination={{
-                            clickable: true,
-                        }}
-                        navigation={true}
-                        modules={[Pagination, Navigation]}
-                        className="mySwiper rounded"
-                    >
-                        <SwiperSlide>
-                            <img src="https://swiperjs.com/demos/images/nature-1.jpg" className='img-fluid' />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="https://swiperjs.com/demos/images/nature-2.jpg" className='img-fluid' />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="https://swiperjs.com/demos/images/nature-3.jpg" className='img-fluid' />
-                        </SwiperSlide>
-                    </Swiper>
+            {rentalDetail && (
+                <div className='row align-items-center'>
+                    <div className='col-5'>
+                        <Swiper
+                            slidesPerView={1}
+                            spaceBetween={10}
+                            loop={true}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            navigation={true}
+                            modules={[Pagination, Navigation]}
+                            className="mySwiper rounded"
+                        >
+                            {rentalDetail.roomImages.map((image: any) => (
+                                <SwiperSlide key={image.id}>
+                                    <img src={image.link} alt="Room" className="img-fluid" />
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+                    </div>
+                    <div className='col-7 container'>
+                        <div className='row mb-3'>
+                            <div className='col-6'>
+                                <div className='motel text-dark'>
+                                    {rentalDetail.motelName}
+                                </div>
+                                <div className='address text-dark'>
+                                    {rentalDetail.motelAdress}
+                                </div>
+                                <div className='room text-dark'>
+                                    {rentalDetail.roomNumber}
+                                </div>
+                                <div className='price text-dark'>
+                                    Giá thuê: {rentalDetail.price} VNĐ
+                                </div>
+                                <div className='area text-dark'>
+                                    Diện tích: {rentalDetail.area} m²
+                                </div>
+                            </div>
+                            <div className='col-6'>
+                                <div className='service-title text-dark'>
+                                    Dịch vụ tiện ích
+                                </div>
+                                <div className='service text-dark'>
+                                    Điện: {rentalDetail.electricPrice} vnd
+                                </div>
+                                <div className='service text-dark'>
+                                    Nước: {rentalDetail.waterPrice} vnd
+                                </div>
+                                {rentalDetail.otherService.map((service: any, index: number) => (
+                                    <div key={index} className="service text-dark">
+                                        {service.name}: {service.price} vnđ
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                        <div className='row mb-3'>
+                            <div className='col-6'>
+                                <div className='infomation-title text-dark'>
+                                    Người thuê
+                                </div>
+                                <div className='infomation text-dark'>
+                                    {rentalDetail.fullName}
+                                </div>
+                                <div className='infomation text-dark'>
+                                    Ngày thuê: {new Date(rentalDetail.createDate).toLocaleDateString()}
+                                </div>
+                                <div className='infomation text-dark'>
+                                    Tháng {new Date(rentalDetail.createDate).getMonth() + 1}:
+                                    {rentalDetail.status ? 'Đã thanh toán' : 'Chưa thanh toán'}
+                                </div>
+                            </div>
+                            <div className='col-6'>
+                                <div className='owner-title text-dark'>
+                                    Liên hệ chủ trọ
+                                </div>
+                                <div className='owner text-dark'>
+                                    {rentalDetail.owner}
+                                </div>
+                                <div className='owner text-dark'>
+                                    SDT: {rentalDetail.phone}
+                                </div>
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='col-6'>
+                                <button className='btn btn-primary col-10'>Thêm người thuê</button>
+                            </div>
+                            <div className='col-6'>
+                                <button className='btn btn-primary col-10'>Phản hồi</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className='col-7 container'>
-                    <div className='row mb-3'>
-                        <div className='col-6'>
-                            <div className='motel text-dark'>
-                                Dũng Hồ
-                            </div>
-                            <div className='address text-dark'>
-                                Địa chỉ: 12 phạm hùng, buôn ma thuột
-                            </div>
-                            <div className='room text-dark'>
-                                Phòng: 202
-                            </div>
-                            <div className='price text-dark'>
-                                Giá thuê: 1200000 vnd
-                            </div>
-                            <div className='area text-dark'>
-                                Diện tích: 12 m2
-                            </div>
-                        </div>
-                        <div className='col-6'>
-                            <div className='service-title text-dark'>
-                                Dịch vụ tiện ích
-                            </div>
-                            <div className='service text-dark'>
-                                Điện: 3000 vnd
-                            </div>
-                            <div className='service text-dark'>
-                                Nước: 10000 vnd
-                            </div>
-                            <div className='service text-dark'>
-                                Wifi: 120000 vnd
-                            </div>
-                        </div>
-                    </div>
-                    <div className='row mb-3'>
-                        <div className='col-6'>
-                            <div className='infomation-title text-dark'>
-                                Người thuê
-                            </div>
-                            <div className='infomation text-dark'>
-                                Hồ dũng
-                            </div>
-                            <div className='infomation text-dark'>
-                                Ngày thuê: 21/11/2024
-                            </div>
-                            <div className='infomation text-dark'>
-                                Tháng 11: Chưa thanh toán
-                            </div>
-                        </div>
-                        <div className='col-6'>
-                            <div className='owner-title text-dark'>
-                                Liên hệ chủ trọ
-                            </div>
-                            <div className='owner text-dark'>
-                                Đỗ Hoàng phong
-                            </div>
-                            <div className='owner text-dark'>
-                                SDT: 0123456780
-                            </div>
-                        </div>
-                    </div>
-                    <div className='row'>
-                        <div className='col-6'>
-                            <button className='btn btn-primary col-10'>Thêm người thuê</button>
-                        </div>
-                        <div className='col-6'>
-                            <button className='btn btn-primary col-10'>Phản hồi</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            )}
         </div>
+
     )
 }
 
